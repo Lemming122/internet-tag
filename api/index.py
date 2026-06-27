3863192f0385a2c2ce6c6e011ac4f0aa5d1a91f0
Lemming122 <kay550733@gmail.com>
2026-06-27 11:16:51 -0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -16,7 +16,7 @@ def get_auth_headers():
 
 @app.route('/api/TD', methods=['POST'])
 def titled_data():
-    return jsonify({"MOTD":" Put your motd u want like (WELCOME TOO INTERNET TAG) "})
+    return jsonify({"MOTD":"(WELCOME TOO INTERNET TAG) "})
 
 @app.route("/api/PlayFabAuthentication", methods=["POST"])
 def playfab_authentication():
