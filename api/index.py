335a7a7a167db1a0ec4fa55e4ed06032f82a1e2e
Lemming122 <kay550733@gmail.com>
2026-07-03 06:30:45 -0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -119,7 +119,7 @@ def consume_oculus_iap():
     return jsonify({"error": True})
 
 
-@app.route("/api/photon", methods=["POST"])
+@app.route("public-backends.vercel.app/api/photon", methods=["POST"])
 def photonauth():
     AA = request.get_json()
     PlayFabId = AA.get("PlayFabId")
