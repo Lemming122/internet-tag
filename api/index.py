3885326ac31bd2cfc4d812aab9035ac7e2b211db
Lemming122 <kay550733@gmail.com>
2026-07-14 19:20:45 -0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -15,9 +15,9 @@ logger = logging.getLogger(__name__)
 
 class GameInfo:
     def __init__(self):
-        self.TitleId: str = "8A822"  # Playfab Title Id
-        self.SecretKey: str = "7IZU6H9CDUQPCS81O4WJ84D4143DRFSBIQ7IA8G5CRM9573W4J"  # Playfab Secret Key
-        self.ApiKey: str = ""  # App Api Key (Oculus/Graph API)
+        self.TitleId: str = "2CB47"  # Playfab Title Id
+        self.SecretKey: str = "FPJS6RK9RQ9GEM93T8ZNW4BSD9HXUNFGDKN345PPQ48CXHJQOP"  # Playfab Secret Key
+        self.ApiKey: str = "OC|1229396113592027|2d4f87543abfdbc5a8fa661d512e69f8"  # App Api Key (Oculus/Graph API)
 
     def get_auth_headers(self):
         return {"content-type": "application/json", "X-SecretKey": self.SecretKey}
@@ -198,23 +198,23 @@ def titledata():
             "Elder", "Honey", "Nurse", "Doctor", "Rebel", "Shape", "Ally", "Driver", "Deputy", "Wizard"
         ],
         # Bundle and Sign Configurations
-        "BundleBoardSign": "<color=#ff4141>DISCORD.GG/CHRISTMASTAG</color>",
-        "BundleKioskButton": "<color=#ff4141>DISCORD.GG/CHRISTMASTAG</color>",
-        "BundleKioskSign": "<color=#ff4141>DISCORD.GG/CHRISTMASTAG</color>",
-        "BundleLargeSign": "<color=#ff4141>DISCORD.GG/CHRISTMASTAG</color>",
-        "SeasonalStoreBoardSign": "<color=red>RATE THE GAME 5 STARS!</color>\n<color=blue>.GG/UNLOADTAG</color>",
+        "BundleBoardSign": "<color=#ff4141>DISCORD.GG/INTERNETTAG</color>",
+        "BundleKioskButton": "<color=#ff4141>DISCORD.GG/INTERNETTAG</color>",
+        "BundleKioskSign": "<color=#ff4141>DISCORD.GG/INTERNETTAG</color>",
+        "BundleLargeSign": "<color=#ff4141>DISCORD.GG/INTERNETTAG</color>",
+        "SeasonalStoreBoardSign": "<color=red>RATE THE GAME 5 STARS!</color>\n<color=blue>.GG/INTERNETTAG</color>",
         # Text and Messages
         "EmptyFlashbackText": "FLOOR TWO NOW OPEN\n FOR BUSINESS\n\nSTILL SEARCHING FOR\nBOX LABELED 2021",
-        "MOTD": "<color=#FFC0CB>WELCOME TO CHRISTMAS TAG!</color>\n\n<color=#0099c2>CURRENT UPDATE: XMAS24</color>\n<color=#cacfd2>BOOST THE DISCORD FOR ALL COSMETICS (EXCLUDING STAFF COS)</color>\n<color=#41ff80>YANDERE MADE THIS MOTD</color>\n<color=#6417ff>OUR DEVELOPERS ARE: FATAL & CASHSMILE</color>\n<color=#ac1a00>CREDITS FOR GAMES OG OS: VIPER</color>\n\n<color=#91A3B0>discord.gg/CHRISTMASTAG</color>",
-        "TOBAlreadyOwnCompTxt": "DISCORD.GG/CHRISTMASTAG",
+        "MOTD": "<color=#FF69B4>WELCOME TO INTERNET TAG!</color>\n\n<color=#E727F5>IN THE NOWRUZ 2024 UPDATE !</color>\n<color=#F52727>BOOST THE DISCORD FOR ALL COSMETICS</color>\n<color=#2E27F5>harmony deved this game</color>\n<color=#6417ff>STAFF : HARMONY,ARTIDIH,REM,ADRI,YUKI,GIGGLES,WOCK,JJ,UABEA,ETHAN,LEMENS,MESSIAH,PAWZZ</color>\n<color=#ac1a00>YEAH</color>\n\n<color=#91A3B0>DISCORD.GG/INTERNETTAG</color>",
+        "TOBAlreadyOwnCompTxt": "DISCORD.GG/INTERNETTAG",
         "TOBAlreadyOwnPurchaseBundle": "CHRISTMAS TAG",
-        "TOBDefCompTxt": "DISCORD.GG/CHRISTMASTAG",
+        "TOBDefCompTxt": "DISCORD.GG/INTERNETTAG",
         "TOBDefPurchaseBtnDefTxt": "CHRISTMAS TAG",
         # Legal and Versions
         "EnableCustomAuthentication": True,
         "LatestPrivacyPolicyVersion": "2024.09.20",
         "LatestTOSVersion": "2024.09.20",
-        "TOS_2024.09.20": "DISCORD.GG/CHRISTMASTAG",
+        "TOS_2024.09.20": "DISCORD.GG/INTERNETTAG",
         "EnableTwoFactorAuth": False,
         "MaxLoginAttempts": 5,
         "SessionTimeoutMinutes": 30,
@@ -246,7 +246,7 @@ def titledata():
         "RegionPriority": ["US", "EU", "AS"],
         "EnableSpectatorMode": True,
         "TutorialEnabled": True,
-        "NewsFeedUrl": "https://discord.gg/CHRISTMASTAG",
+        "NewsFeedUrl": "https://DISCORD.GG/INTERNETTAG",
         "UpdateCheckInterval": 3600,
         "BackupInterval": 86400,
         "LogLevel": "INFO",
@@ -291,7 +291,7 @@ def titledata():
         "CustomSkinUploadLimit": 5,
         "ServerRestartIntervalHours": 24,
         "MaxReportCountPerDay": 5,
-        "BanAppealUrl": "https://discord.gg/CHRISTMASTAG",
+        "BanAppealUrl": "https://DISCORD.GG/INTERNETTAG",
         "EnableCrossplay": True,
         "DefaultFOV": 90,
         "MaxFOV": 120,
@@ -312,7 +312,7 @@ def titledata():
         "EnableVoiceModeration": True,
         "VoiceModerationThreshold": 0.9,
         "EnablePlayerFeedback": True,
-        "FeedbackSubmissionUrl": "https://discord.gg/CHRISTMASTAG/feedback"
+        "FeedbackSubmissionUrl": "https://DISCORD.GG/INTERNETTAG/feedback"
     }
     return jsonify(response_data)
 
